dd54428c732deb166e6293573b95e7cea0d615bd
wongprom <jimmybackstrom@hotmail.com>
2019-08-25 13:24:58 +0200

    Updates

---
+++ precache-manifest.d4d5a8bea3072d7b4a915eacc0780033.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b1976edb7fdae2efc9e4219d57e55fb",
+    "url": "/robot-friends/index.html"
+  },
+  {
+    "revision": "eba9d2d89df6e3a68a60",
+    "url": "/robot-friends/static/css/2.e40df323.chunk.css"
+  },
+  {
+    "revision": "e31382bd6832150894b8",
+    "url": "/robot-friends/static/css/main.0085faef.chunk.css"
+  },
+  {
+    "revision": "eba9d2d89df6e3a68a60",
+    "url": "/robot-friends/static/js/2.c733c26a.chunk.js"
+  },
+  {
+    "revision": "e31382bd6832150894b8",
+    "url": "/robot-friends/static/js/main.402a93d6.chunk.js"
+  },
+  {
+    "revision": "f8c63846f1fb9c939854",
+    "url": "/robot-friends/static/js/runtime~main.88802e26.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robot-friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
